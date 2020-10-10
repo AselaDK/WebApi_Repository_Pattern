fe119e90448a83d654edbea581a7b5bc97dbc0e6
aseladk <aselax2@gmail.com>
2020-10-10 17:28:57 +0530

    data access layer created OK

---
+++ Domain/Developer.cs
@@ -0,0 +1,14 @@
+﻿using System;
+
+namespace Domain
+{
+    //Now, let’s add the Required Entities to the Domain Project. Create a new Folder in the Domain Project named Entities.
+    public class Developer
+    {
+        //Create 2 very simple classes – Developer and Project under the Entities Folder.
+
+        public int Id { get; set; }
+        public string Name { get; set; }
+        public int Followers { get; set; }
+    }
+}
---
+++ Domain/Project.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Domain
+{
+    public class Project
+    {
+        public int Id { get; set; }
+        public string Name { get; set; }
+    }
+}
+
+// Next , we will setup and configure Entity Framework Core. Install these Required Packages in the DataAccess.EFCore Project. Here is where we would have our DbContect Class and the actual Implementations of the Repositories.
