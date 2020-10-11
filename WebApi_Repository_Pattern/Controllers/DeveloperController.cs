eb56978e6cb28b2f10818aaeb9a44edb38515ae5
aseladk <aselax2@gmail.com>
2020-10-11 14:06:16 +0530

    added a controller and injected IUnitOfWork object to that controller

---
+++ WebApi_Repository_Pattern/Controllers/DeveloperController.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Domain.Interfaces;
+using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+
+namespace WebApi_Repository_Pattern.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class DeveloperController : ControllerBase
+    {
+        // injecting only the IUnitOfWork object
+
+        private readonly IUnitOfWork _unitOfWork;
+        public DeveloperController(IUnitOfWork unitOfWork)
+        {
+            _unitOfWork = unitOfWork;
+        }
+    }
+}
